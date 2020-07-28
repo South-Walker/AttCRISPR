65e09de56fc7f0f8713f0180ee235352ada3efe0
South-Walker <506830915@qq.com>
2020-07-28 17:16:05 +0800

    7-28 haha i'm a drawer

---
+++ PaperDrawer/Assets/BlendCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+[ExecuteInEditMode]
+public class BlendCamera : MonoBehaviour
+{
+    public RenderTexture rt_posteff;
+    public Shader blend;
+    private Material m_blend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (m_blend == null)
+        {
+            m_blend = new Material(blend);
+        }
+        m_blend.SetTexture("_PosteffTex", rt_posteff);
+        m_blend.SetTexture("_SourceTex", source);
+        Graphics.Blit(source, destination, m_blend);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
