f15e5d2192f665f358f3cb78277a52dfe7d0b358
xiao <xlm@xiaoliming96.com>
2020-08-02 19:35:36 +0800

    8_2 drawer

--- PaperDrawer/ConvolutionTexture/ConvolutionTexture/Program.cs
+++ PaperDrawer/ConvolutionTexture/ConvolutionTexture/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace ConvolutionTexture
 {
@@ -12,50 +13,110 @@ namespace ConvolutionTexture
     {
         static StringBuilder rootPath { get { return new StringBuilder(@"../../../../DrawerData/"); } }
         static string sgRNADirectory = rootPath.Append("Convolution/").ToString();
-        static string outputPath = rootPath.Append("convolution{0}.png").ToString(); 
+        static string allinput = rootPath.Append("Convolution/singleall.out").ToString();
+        static string outputPath = rootPath.Append("convolution{0}.png").ToString();
         static void Main(string[] args)
         {
-            DirectoryInfo dirinfo = new DirectoryInfo(sgRNADirectory);
-            int imgcount = 0;
-            foreach (var fileinfo in dirinfo.GetFiles())
+            double[,,] datas = new double[21, 4, 60];
+            using (FileStream fs = new FileStream(allinput, FileMode.Open, FileAccess.Read))
             {
-                StreamReader sr = fileinfo.OpenText();
+                StreamReader sr = new StreamReader(fs);
                 var ss = sr.ReadToEnd().Split(',');
                 int ssindex = 0;
-                double[,] convoutput = new double[21, 4];
+                for (int i = 0; i < datas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < datas.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < datas.GetLength(2); k++)
+                        {
+                            datas[i, j, k] = Convert.ToDouble(ss[ssindex++]);  
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < datas.GetLength(2); k++)
+            {
                 double max = double.MinValue;
                 double min = double.MaxValue;
-                for (int i = 0; i < convoutput.GetLength(0); i++)
+                for (int i = 0; i < datas.GetLength(0); i++)
                 {
-                    for (int j = 0; j < convoutput.GetLength(1); j++)
+                    for (int j = 0; j < datas.GetLength(1); j++)
                     {
-                        convoutput[i, j] = Convert.ToDouble(ss[ssindex++]);
-                        max = (max > convoutput[i, j]) ? max : convoutput[i, j];
-                        min = (min > convoutput[i, j]) ? convoutput[i, j] : min;
+                        max = (max > datas[i, j, k]) ? max : datas[i, j, k];
+                        min = (min > datas[i, j, k]) ? datas[i, j, k] : min;
                     }
                 }
-                double scale = 1.0 / (max - min);
-                for (int i = 0; i < convoutput.GetLength(0); i++)
+                double scale = (max - min);
+                for (int i = 0; i < datas.GetLength(0); i++)
                 {
-                    for (int j = 0; j < convoutput.GetLength(1); j++)
+                    for (int j = 0; j < datas.GetLength(1); j++)
                     {
-                        convoutput[i, j] += min;
-                        convoutput[i, j] *= scale;
+                        datas[i, j, k] += min;
+                        if (scale == 0)
+                            datas[i, j, k] = 0.5;
+                        else
+                            datas[i, j, k] /= scale;
                     }
                 }
-                Bitmap img = new Bitmap(1024, 1024);
+            }
+            CudeTexture(datas, 3, 20);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <param name="outputdimnum"></param>
+        /// 一个conv输出的维度
+        /// <param name="inputdimnum"></param>
+        static void CudeTexture(double[,,] datas, int outputdimnum, int inputdimnum)
+        {
+            int offsetk = 10;
+            int totaldim = datas.GetLength(2);
+            int cudecount = totaldim / inputdimnum;
+            Bitmap img = new Bitmap(1024, 1024);
+            for (int i = 0; i < cudecount; i++)
+            {
+                int kbegin = i * inputdimnum + offsetk;
                 for (int w = 0; w < img.Width; w++)
                 {
                     for (int h = 0; h < img.Height; h++)
                     {
-                        int i = w * convoutput.GetLength(0) / (img.Width + 1);
-                        int j = h * convoutput.GetLength(1) / (img.Height + 1);
-                        int grey = (int)(convoutput[i, j] * 255);
+                        int x = w * datas.GetLength(0) / (img.Width + 1);
+                        int y = h * datas.GetLength(1) / (img.Height + 1);
+                        int grey = (int)(datas[x, y, kbegin] * 255);
                         img.SetPixel(h, w, Color.FromArgb(grey, grey, grey));
                     }
                 }
-                img.Save(string.Format(outputPath, imgcount));
-                imgcount++;
+                img.Save(string.Format(outputPath,
+                    string.Format("{0}_{1}", i, "front")
+                    ));
+                for (int w = 0; w < img.Width; w++)
+                {
+                    for (int h = 0; h < img.Height; h++)
+                    {
+                        int x = w * outputdimnum / (img.Width + 1);
+                        int y = h * datas.GetLength(1) / (img.Height + 1);
+                        int grey = (int)(datas[0, y, kbegin + x] * 255);
+                        img.SetPixel(h, w, Color.FromArgb(grey, grey, grey));
+                    }
+                }
+                img.Save(string.Format(outputPath,
+                    string.Format("{0}_{1}", i, "right")
+                    ));
+
+                for (int w = 0; w < img.Width; w++)
+                {
+                    for (int h = 0; h < img.Height; h++)
+                    {
+                        int x = w * datas.GetLength(0) / (img.Width + 1);
+                        int y = h * outputdimnum / (img.Height + 1);
+                        int grey = (int)(datas[x, 0, kbegin + y] * 255);
+                        img.SetPixel(h, w, Color.FromArgb(grey, grey, grey));
+                    }
+                }
+                img.Save(string.Format(outputPath,
+                    string.Format("{0}_{1}", i, "up")
+                    ));
             }
         }
     }
