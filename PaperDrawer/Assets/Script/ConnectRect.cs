c86fa8d11d5bb42032b8fd56752c3bddee584bb7
South-Walker <506830915@qq.com>
2020-08-04 22:24:17 +0800

    8_4 drawer

--- PaperDrawer/Assets/Script/ConnectRect.cs
+++ PaperDrawer/Assets/Script/ConnectRect.cs
@@ -2,40 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 [ExecuteInEditMode]
 public class ConnectRect : MonoBehaviour
 {
-    public GameObject cude1;
-    public GameObject cude2;
-    Matrix4x4 cude1object2world, cude2object2world, cude1world2screen,cude2world2screen;
-    public Camera cam1, cam2;
-    Vector4 cude1frontbottomright, cude1fronttopright;
-    Vector4 cude2frontbottomleft, cude2fronttopleft;
-    Matrix4x4 cude1MVP;
-    Matrix4x4 cude2MVP;
+    public GameObject cude1,cude2;
+    public Camera cam1, cam2, maincam;
+    public Vector2 offset1;
+    public Material mat;
+    private Vector2 cude1FrontTopRightinScreen;
+    private Vector2 cude1FrontBottomRightinScreen;
+    private Vector2 cude2FrontTopLeftScreen;
+    private Vector2 cude2FrontBottomLeftScreen;
+
     // Start is called before the first frame update
     void Start()
     {
+        cude1FrontTopRightinScreen = GetScreenPos(new Vector4(0.5f, 0.5f, 0.5f, 1), cude1, cam1, offset1);
+        cude1FrontBottomRightinScreen = GetScreenPos(new Vector4(0.5f, -0.5f, 0.5f, 1), cude1, cam1, offset1);
+        cude2FrontTopLeftScreen = GetScreenPos(new Vector4(-0.5f, 0.5f, 0.5f, 1), cude2, cam2);
+        cude2FrontBottomLeftScreen = GetScreenPos(new Vector4(-0.5f, -0.5f, 0.5f, 1), cude2, cam2);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        cude1object2world = cude1.transform.localToWorldMatrix;
-        cude2object2world = cude2.transform.localToWorldMatrix;
-        cude1world2screen = cam1.projectionMatrix;
-        cude2world2screen = cam2.projectionMatrix;
-        cude1MVP = cude1object2world * cude1world2screen;
-        cude2MVP = cude2object2world * cude2world2screen;
-        cude1frontbottomright = new Vector4(0.5f, 0.5f, 0.5f, 1);
-        cude1fronttopright = new Vector4(0.5f, -0.5f, 0.5f, 1);
-        cude2frontbottomleft = new Vector4(0.5f, 0.5f, 0.5f, 1);
-        cude2fronttopleft = new Vector4(0.5f, -0.5f, 0.5f, 1);
-        cude1frontbottomright = cude1MVP * cude1frontbottomright;
-        cude1fronttopright = cude1MVP * cude1fronttopright;
-        cude2frontbottomleft = cude2MVP * cude2frontbottomleft;
-        cude2fronttopleft = cude2MVP * cude2fronttopleft;
-        Debug.Log(cude1frontbottomright);
+    }
+    private void OnPostRender()
+    {
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        mat.SetPass(0);
+        GL.Begin(GL.LINES);
+
+        GL.Vertex3(cude1FrontTopRightinScreen.x / maincam.pixelWidth, 1-cude1FrontTopRightinScreen.y / maincam.pixelHeight, 0);
+        GL.Vertex3(cude2FrontTopLeftScreen.x / maincam.pixelWidth, 1-0.5f-cude2FrontTopLeftScreen.y / maincam.pixelHeight, 0);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+    Vector2 GetScreenPos(Vector4 objectpos, GameObject game, Camera cam)
+    {
+        return GetScreenPos(objectpos, game, cam, Vector2.zero);
+    }
+    Vector2 GetScreenPos(Vector4 objectpos, GameObject game, Camera cam, Vector2 offset)
+    {
+        Vector3 worldPos = game.transform.localToWorldMatrix * objectpos;
+        Vector2 camscreenPos = cam.WorldToScreenPoint(worldPos);
+        Vector2 mainscreenPos = new Vector2(camscreenPos.x - offset.x * cam.pixelWidth,
+            cam.pixelHeight - camscreenPos.y + offset.y * cam.pixelHeight);
+        return mainscreenPos;
     }
 }
