c49a0225c1f0681f2210cdfb0178ec0763a7f7dc
xiao <xlm@xiaoliming96.com>
2020-10-26 20:55:12 +0800

    10-26 paper&attention code

--- Train/GetModelAttention.py
+++ Train/GetModelAttention.py
@@ -9,81 +9,67 @@ from sklearn.model_selection import train_test_split
 import os
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-pkl = open('WTData.pkl','rb') 
-x_onehot =  pickle.load(pkl)
-x_biofeat = pickle.load(pkl)
-y = pickle.load(pkl)
-x_seq = pickle.load(pkl)
-
-random_state=40
-test_size = 0.15
-x_train_onehot, x_test_onehot, y_train, y_test = train_test_split(x_onehot, y, test_size=test_size, random_state=random_state)
-x_train_biofeat, x_test_biofeat, y_train, y_test = train_test_split(x_biofeat, y, test_size=test_size, random_state=random_state)
-x_train_seq, x_test_seq, y_train, y_test = train_test_split(x_seq, y, test_size=test_size, random_state=random_state)
 
 from keras.callbacks import Callback
 from keras.callbacks import LambdaCallback
-x_attention_onehot = x_train_onehot
-x_attention_biofeat = x_train_biofeat
-x_attention_seq = x_train_seq
-y_attention = y_train
-def get_local_temporal_attention(model):
-    temporal_layer_model = Model(inputs=model.inputs[0],outputs=model.get_layer('temporal_attention').output)
-    temporal_attention = temporal_layer_model.predict(x_attention_onehot)
-    temporal_value = np.mean(temporal_attention, axis=0)
-    ##normalize
-    for i in range(21):
-        sum = 0
-        for j in range(21):
-            sum+=temporal_value[i][j]
-        for j in range(21):
-            temporal_value[i][j] = temporal_value[i][j]/sum
-    answer = []
-    sums = []
-    for i in range(21):
-        sum = 0
-        for j in range(21):
-            sum += temporal_value[i][j]
-        sums.append(sum)
-    for i in range(21):
-        preanswer = []
-        for k in range(21):
-            time = 10*temporal_value[i][k]/sums[i]+0.5
-            time = int(time)
-            for t in range(time):
-                answer.append('A'+str(i)+','+'B'+str(k))
-    return answer
+x_attention_onehot = None
+x_attention_biofeat = None
+x_attention_seq = None
+y_attention = None
+def ReadData(dataset):
+    dict = {'WT':'WTData.pkl','ESP':'ESPData.pkl','SP':'SPData.pkl'}    
+    pkl = open(dict[dataset],'rb') 
+    x_onehot =  pickle.load(pkl)
+    x_biofeat = pickle.load(pkl)
+    y = pickle.load(pkl)
+    x_seq = pickle.load(pkl)
+    global x_attention_onehot,x_attention_biofeat,x_attention_seq,y_attention
+    x_attention_onehot = x_onehot
+    x_attention_biofeat = x_biofeat
+    x_attention_seq = x_seq
+    y_attention = y
+def get_local_temporal_attention(model,index):
+    seq = x_attention_seq[index]
+    onehot = x_attention_onehot[index].reshape((1,21,4,1))
+    print('check:'+str(seq))
+    
+    last_layer_weight = model.get_layer('temporal_score').get_weights()[0].reshape((-1))
+    temporal_layer_model = Model(
+        inputs=model.inputs[0], outputs=model.get_layer('score_at_each_position').output)
+    ##(datasize,21,4,1)
+    score_at_each_position = temporal_layer_model.predict(onehot)
+    baseline = np.mean(score_at_each_position, axis=0)
+
+    baseline = np.multiply(baseline,last_layer_weight)
+    
+    second_layer_model = Model(
+        inputs=model.inputs[0],outputs=model.get_layer('temporal_attention').output)
+    #second = second_layer_model.predict(onehot)
+    second = second_layer_model.predict(onehot)
+    second = np.mean(second,axis=0)
+    baseline = np.matmul(baseline,second)
+    np.savetxt(str(index)+'.csv',baseline,fmt='%.5f',delimiter=',')
+    return None
 def get_temporal_attention(model):
-    temporal_layer_model = Model(inputs=model.inputs[0], outputs=model.get_layer('temporal_attention').output)
-    temporal_score_model = Model(inputs=model.inputs[0], outputs=model.get_layer('last_score').output)
-    temporal_perpos_score_weight = model.get_layer('temporal_score').get_weights()
-    last_layer_weight = model.get_layer('temporal_score').get_weights()[0]
-    temporal_attention = temporal_layer_model.predict(x_attention_onehot)
-    temporal_value = np.mean(temporal_attention, axis=0)
-    temporal_score = np.mean(temporal_score_model.predict(x_attention_onehot),axis=0)
-    #can't know
-    for i in range(21):
-        sum = 0
-        count = 0
-        ##normalize
-        for j in range(21):
-            sum += temporal_value[i][j]
-        for j in range(21):
-            temporal_value[i][j] = temporal_value[i][j]/sum
-            #temporal_value[i][j] = temporal_value[i][j]/temporal_score[i]
-        ##zscore
-        #for j in range(21):
-            #sum += temporal_value[i][j]
-            #count += 1 if temporal_value[i][j]!=0 else 0
-        #avg = sum / count
-        #sumx_2 = 0
-        #for j in range(21):
-            #sumx_2 += (temporal_value[i][j]-avg)*(temporal_value[i][j]-avg) if temporal_value[i][j]!=0 else 0
-        #sigma = math.sqrt(sumx_2 / count)
-        #for j in range(21):
-            #temporal_value[i][j] = (temporal_value[i][j]-avg) / sigma if temporal_value[i][j]!=0 else 0
-            #temporal_value[i][j] = temporal_value[i][j]/temporal_score[i]
-    return temporal_value
+    
+    second_layer_model = Model(
+        inputs=model.inputs[0],outputs=model.get_layer('temporal_attention').output)
+    #second = second_layer_model.predict(onehot)
+    second = second_layer_model.predict(x_attention_onehot)
+    second = np.mean(second,axis=0)
+    scale = np.sum(second)/21
+
+
+    last_layer_weight = model.get_layer('temporal_score').get_weights()[0].reshape((-1))
+    temporal_layer_model = Model(
+        inputs=model.inputs[0], outputs=model.get_layer('score_at_each_position').output)
+    ##(datasize,21)
+    score_at_each_position = temporal_layer_model.predict(x_attention_onehot)
+    baseline = np.mean(score_at_each_position, axis=0)
+    baseline = np.multiply(baseline,last_layer_weight)
+    baseline = baseline * scale
+    np.savetxt('baseline.csv',baseline,fmt='%.5f',delimiter=',')
+    return None
 def get_spatial_attention(model):
     spatial_layer_model = Model(
         inputs=model.inputs[0], outputs=model.get_layer('spatial_attention_result').output)
@@ -93,15 +79,31 @@ def get_spatial_attention(model):
     spatial_value = np.mean(spatial_value, axis=0)
     spatial_sum = np.sum(spatial_value, axis=1)
     for i in range(len(spatial_value)):
-        t = spatial_sum[i]/4
-        if i == 0:
-            t*=2
-        for j in range(len(spatial_value[0])):
-            spatial_value[i][j] -= t
+        ZScore(spatial_value[i])
+        #t = spatial_sum[i]/4
+        #if i == 0:
+        #    t*=2
+        #for j in range(len(spatial_value[0])):
+        #    spatial_value[i][j] -= t
     spatial_value[0][1]=0
     spatial_value[0][3]=0
     return spatial_value.T
-
+def ZScore(list):
+    sum = 0
+    l = len(list)
+    nonzero=0
+    for i in range(l):
+        if list[i]!=0:
+            nonzero+=1
+    for i in range(l):
+        sum += list[i]
+    avg = sum / nonzero
+    sumx_2 = 0
+    for i in range(l):
+        sumx_2 += (list[i]-avg)*(list[i]-avg) if list[i]!=0 else 0
+    sigma = math.sqrt(sumx_2 / nonzero)
+    for i in range(l):
+        list[i] = (list[i]-avg) / sigma if list[i]!=0 else 0
 def bezierdim1(t,points,pointsnum=4):
     if t<0 or t>1:
         print('wrong')
@@ -118,7 +120,6 @@ def rec(n,m):
         return n
     else:
         return rec(n-1,m-1)+rec(n-1,m)
-    
 def bezier(t,points,pointsnum): 
     tpow = []
     oneminuestpow = []
@@ -137,7 +138,6 @@ def bezier(t,points,pointsnum):
         r+=points[i]*tpow[i]*oneminuestpow[pointsnum-1-i]*cnums[i]
 
     return r
-
 def Plot3D(spatial_attention):
     from matplotlib import pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
@@ -146,7 +146,7 @@ def Plot3D(spatial_attention):
     ax1 = plt.axes(projection='3d')
 
     import numpy as np
-    xstep = 0.5
+    xstep = 0.05
     ystep = 0.2
     xbegin = 1.0
     ybegin = 0.0
@@ -178,9 +178,10 @@ def Plot3D(spatial_attention):
             minvalue = Z[i][j] if Z[i][j]<minvalue else minvalue
             maxvalue = Z[i][j] if Z[i][j]>maxvalue else maxvalue
     x_scale_ls = [i+1 for i in range(4)]
-    x_index_ls = ['T','A','C','G']
+    x_index_ls = ['C','A','T','G']
     y_scale_ls = [2*i for i in range(11)]
-    y_index_ls = [str(2*i+1) for i in range(11)]
+    y_index_ls = [str(2*i+1) for i in range(10)]
+    y_index_ls.append('N')
     ax1.plot_surface(X,Y,Z,cmap='rainbow')
     ax1.contour(X,Y,Z,zdim='z',offset=minvalue,cmap='rainbow')
     distance = maxvalue-minvalue
@@ -194,40 +195,24 @@ def Plot3D(spatial_attention):
     ax1.set_zticklabels(z_index_ls)
     ax1.set_xlabel('Base') 
     ax1.set_ylabel('Position')
-    ax1.view_init(elev=17., azim=-141)
+    ax1.view_init(elev=20., azim=-166)
     plt.show()
-def SinglePred(usebiofeat=True):
-    onepkl = open('GTTGAGAAGGACCGCCACAAC.pkl','rb')
-    onex_seq = pickle.load(onepkl)
-    onex_onehot =  pickle.load(onepkl)
-    onex_biofeat = pickle.load(onepkl)
-    layer_model = Model(
-        inputs=load_model.input, outputs=load_model.get_layer('conv_output').output)
-    if usebiofeat:
-        input = [onex_onehot,onex_biofeat]
-    else:
-        input = [onex_onehot]
-    values = layer_model.predict(input)
-    begin = 0
-    step = 20
 
-    values.tofile('singleall.out',sep=',',format='%s')
 
-ensemble_model = load_model('./WTFineTuning.h5')
-ensemble_model.summary()
-cnn_model = ensemble_model.get_layer('cnn')
-rnn_model = ensemble_model.get_layer('rnn')
-#rnn_model = load_model('./WTBestRNN.h5')
-temporal_attention = get_temporal_attention(rnn_model)
-np.savetxt('global_temporal_attention.csv',temporal_attention,fmt='%.5f',delimiter=',')
-print(temporal_attention)
+rnnmodeldict = {'WT':'WTBestRNN.h5','ESP':'ESPBestRNN.h5','SP':'SPBestRNN.h5'}
+cnnmodeldict = {'WT':'WTBestCNN.h5','ESP':'ESPBestCNN.h5','SP':'SPBestCNN.h5'}
+dataname = 'WT'
+ReadData(dataname)
+rnn_model = load_model('./'+rnnmodeldict[dataname])
+get_local_temporal_attention(rnn_model,7882)
+get_temporal_attention(rnn_model)
+
+
+cnn_model = load_model('./'+cnnmodeldict[dataname])
 spatial_attention = get_spatial_attention(cnn_model)
-print(spatial_attention)
-for i in range(4):
-    for j in range(21):
-        print(str(spatial_attention[i][j]) + ",",end='')
-    print('')
-
-
+np.savetxt('SP_global_attention.csv',spatial_attention,fmt='%.5f',delimiter=',')
 Plot3D(spatial_attention)
+
+print(spatial_attention)
+
 print('end')
